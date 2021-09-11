a3d19d859fe30c91807032cb2e9d4c60af6ae669
Silvio Silva <smsilva@gmail.com>
2021-09-11 17:31:57 -0300

    Exemplo com VNET funcionando

--- examples/one_virtual_network_with_four_subnets/main.tf
+++ examples/one_virtual_network_with_four_subnets/main.tf
@@ -11,9 +11,10 @@ resource "random_string" "platform_instance_id" {
 
 locals {
   platform_instance_name = "crow-sandbox-${random_string.platform_instance_id.result}"
-  location               = "centralus"
 
-  virtual_network_cidrs = ["10.0.0.0/8"]
+  virtual_network_location = "centralus"
+  virtual_network_name     = "vnet-example"
+  virtual_network_cidrs    = ["10.0.0.0/8"]
 
   subnets = [
     { cidr = "10.0.1.0/29", name = "AzureBastionSubnet" },
@@ -21,18 +22,14 @@ locals {
     { cidr = "10.0.3.0/29", name = "firewall" },
     { cidr = "10.240.0.0/16", name = "aks" },
   ]
-
-  subnets_map = {
-    for subnet in local.subnets : subnet.name => subnet
-  }
 }
 
 module "vnet_example" {
   source = "../../src/vnet"
 
   platform_instance_name = local.platform_instance_name
-  location               = local.location
-  name                   = "vnet-example"
+  location               = local.virtual_network_location
+  name                   = local.virtual_network_name
   cidrs                  = local.virtual_network_cidrs
   subnets                = local.subnets
 }
