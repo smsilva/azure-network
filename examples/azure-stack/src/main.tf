63d0e6ea1250de13af2419bdada827450003f16f
Silvio Silva <smsilva@gmail.com>
2021-11-13 13:43:18 -0300

    Creating AKS

--- examples/azure-stack/src/main.tf
+++ examples/azure-stack/src/main.tf
@@ -1,12 +1,14 @@
-data "azurerm_resource_group" "default" {
-  name = var.resource_group_name != "" ? var.resource_group_name : var.name
-}
-
 locals {
   virtual_network_cidrs = ["10.0.0.0/8"]
   virtual_network_subnets = [
     { cidr = "10.140.0.0/16", name = "AzureBastionSubnet" }
   ]
+
+  resource_group_name = var.resource_group_name != "" ? var.resource_group_name : var.name
+}
+
+data "azurerm_resource_group" "default" {
+  name = local.resource_group_name
 }
 
 module "vnet_example" {
